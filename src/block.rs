4bee0ef29042ae7d71cd97415b07023275a28093
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-06-09 23:09:22 +0400

    Blok implementasiyasini yazdim

--- src/block.rs
+++ src/block.rs
@@ -1,5 +1,11 @@
-use std::time::SystemTime;
+use std::{time::SystemTime, vec};
 
+use crypto::{digest::Digest, sha2::Sha256};
+use log::info;
+
+pub type Result<T> = std::result::Result<T, failure::Error>;
+
+const TARGET_HEXT: usize = 4;
 pub struct Block {
     timestamp: u128,
     transactions: String,
@@ -30,4 +36,45 @@ impl Block {
         block.run_proof_if_work()?;
         Ok(block)
     }
+
+    fn run_proof_if_work(&mut self) ->Result<()> {
+        info!("Minnning the block");
+        while !self.validate()? {
+            self.nonce +=1;
+        }
+
+        let data = self.prepare_hash_data()?;
+        let mut hasher = Sha256::new();
+        hasher.input(&data[..]);
+        self.hash = hasher.result_str();
+        Ok(())
+    }
+
+    fn prepare_hash_data(&self) -> Result<Vec<u8>> {
+        let content = (
+            self.prev_block_hash.clone(),
+            self.transactions.clone(),
+            self.timestamp,
+            TARGET_HEXT,
+            self.nonce
+        );
+
+        let bytes = bincode::serialize(&content)?;
+        Ok(bytes)
+    }
+
+    fn validate(&self) -> Result<bool> {
+        let data = self.prepare_hash_data()?;
+        let mut hasher = Sha256::new();
+        hasher.input(&data[..]);
+
+        let mut vec1 = vec![];
+        vec1.resize(TARGET_HEXT, '0' as u8);
+        println!("{:?}", vec1);
+
+        Ok(&hasher.result_str()[0..TARGET_HEXT] == String::from_utf8(vec1)?)
+
+    }
 }
+
+
